d690421ccf2b073a49d524d488ea9fc379e308ea
fangyumin <52485958+fangyumin@users.noreply.github.com>
2019-07-03 16:25:27 +0800

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,19 @@
+SELECT * from cst_customer
+
+INSERT INTO cst_customer ( cust_name, cust_source, cust_industry, cust_level, cust_address, cust_phone )
+VALUES
+	( 'struts2名称1', '客户源1', '客户industry1', '1', '地址1', '电话1' );
+	INSERT INTO cst_customer ( cust_name, cust_source, cust_industry, cust_level, cust_address, cust_phone )
+VALUES
+	( 'struts2名称2', '客户源2', '客户industry2', '5', '地址2', '电话2' );
+	INSERT INTO cst_customer ( cust_name, cust_source, cust_industry, cust_level, cust_address, cust_phone )
+VALUES
+	( 'struts2名称3', '客户源3', '客户industry3', '2', '地址3', '电话3' );
+	INSERT INTO cst_customer ( cust_name, cust_source, cust_industry, cust_level, cust_address, cust_phone )
+VALUES
+	( 'struts2名称4', '客户源4', '客户industry4', '3', '地址4', '电话4' );
+	INSERT INTO cst_customer ( cust_name, cust_source, cust_industry, cust_level, cust_address, cust_phone )
+VALUES
+	( 'struts2名称5', '客户源5', '客户industry5', '4', '地址5', '电话5' );
+	INSERT INTO cst_customer ( cust_name, cust_source, cust_industry, cust_level, cust_address, cust_phone )
+VALUES
